28a90e6bb862b93b6e4716b11a7d23347dcf6185
Alvarodevs <alvaro.develops@gmail.com>
2020-12-14 20:33:33 +0000

    Added html+css+js modified

--- src/main.js
+++ src/main.js
@@ -2,9 +2,30 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  let suits = ["spade", "club", "heart", "diamond"];
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  let suit = Math.floor(Math.random() * suits.length - 1);
+  let number = Math.floor(Math.random() * numbers.length - 1);
+
+  var newSuit = document.getElementById("suit");
+  newSuit.innerHTML = suits[suit];
+
+  var newcard = document.getElementById("number");
+  newcard.innerHTML = numbers[number];
 };
