f2c4abc6b13c321939de85f1c387a3cdc5f2fdb7
Alvarodevs <alvaro.develops@gmail.com>
2020-12-15 12:18:52 +0000

    Card layout up to date

--- src/main.js
+++ src/main.js
@@ -3,7 +3,29 @@ import "bootstrap";
 import "./style.css";
 
 window.onload = function() {
-  let suits = ["spade", "club", "heart", "diamond"];
+  let suits = ["spades", "hearts", "clubs", "diamonds"];
+  //     {
+  //       name: "spades",
+  //       image:
+  //         "https://w7.pngwing.com/pngs/667/791/png-transparent-playing-card-poker-suit-spades-gambling-ace-card-king-leaf-hearts.png"
+  //     },
+  //     {
+  //       name: "hearts",
+  //       image:
+  //         "https://w7.pngwing.com/pngs/667/791/png-transparent-playing-card-poker-suit-spades-gambling-ace-card-king-leaf-hearts.png"
+  //     },
+  //     {
+  //       name: "clubs",
+  //       image:
+  //         "https://w7.pngwing.com/pngs/667/791/png-transparent-playing-card-poker-suit-spades-gambling-ace-card-king-leaf-hearts.png"
+  //     },
+  //     {
+  //       name: "diamonds",
+  //       image:
+  //         "https://w7.pngwing.com/pngs/667/791/png-transparent-playing-card-poker-suit-spades-gambling-ace-card-king-leaf-hearts.png"
+  //     }
+  //   ];
+
   let numbers = [
     "A",
     "2",
@@ -20,11 +42,14 @@ window.onload = function() {
     "K"
   ];
 
-  let suit = Math.floor(Math.random() * suits.length - 1);
-  let number = Math.floor(Math.random() * numbers.length - 1);
+  let suit = Math.floor(Math.random() * suits.length);
+  let number = Math.floor(Math.random() * numbers.length);
 
   var newSuit = document.getElementById("suit");
   newSuit.innerHTML = suits[suit];
+
+  var newSuit2 = document.getElementById("suit2");
+  newSuit2.innerHTML = suits[suit];
 
   var newcard = document.getElementById("number");
   newcard.innerHTML = numbers[number];
