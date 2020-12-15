c50c4cb2b77b8589e1064428b5613ec7a37c6907
nitlara <nitlara@gmail.com>
2020-12-15 13:46:38 +0000

    cambios en js añadir diccionario con valores correctos y algun cambio en css sobre tamaño

--- src/main.js
+++ src/main.js
@@ -3,28 +3,40 @@ import "bootstrap";
 import "./style.css";
 
 window.onload = function() {
-  let suits = ["spades", "hearts", "clubs", "diamonds"];
-  //     {
-  //       name: "spades",
-  //       image:
-  //         "https://w7.pngwing.com/pngs/667/791/png-transparent-playing-card-poker-suit-spades-gambling-ace-card-king-leaf-hearts.png"
-  //     },
-  //     {
-  //       name: "hearts",
-  //       image:
-  //         "https://w7.pngwing.com/pngs/667/791/png-transparent-playing-card-poker-suit-spades-gambling-ace-card-king-leaf-hearts.png"
-  //     },
-  //     {
-  //       name: "clubs",
-  //       image:
-  //         "https://w7.pngwing.com/pngs/667/791/png-transparent-playing-card-poker-suit-spades-gambling-ace-card-king-leaf-hearts.png"
-  //     },
-  //     {
-  //       name: "diamonds",
-  //       image:
-  //         "https://w7.pngwing.com/pngs/667/791/png-transparent-playing-card-poker-suit-spades-gambling-ace-card-king-leaf-hearts.png"
-  //     }
-  //   ];
+  let suits = [
+    {
+      name: "spades",
+      sign: "♠",
+      code: "u+2660 ISOpub",
+      color: "black",
+      image:
+        "https://w7.pngwing.com/pngs/667/791/png-transparent-playing-card-poker-suit-spades-gambling-ace-card-king-leaf-hearts.png"
+    },
+    {
+      name: "hearts",
+      sign: "♥",
+      code: "u+2665 ISOpub",
+      color: "red",
+      image:
+        "https://w7.pngwing.com/pngs/667/791/png-transparent-playing-card-poker-suit-spades-gambling-ace-card-king-leaf-hearts.png"
+    },
+    {
+      name: "clubs",
+      sign: "♣",
+      code: "u+2663 ISOpub",
+      color: "black",
+      image:
+        "https://w7.pngwing.com/pngs/667/791/png-transparent-playing-card-poker-suit-spades-gambling-ace-card-king-leaf-hearts.png"
+    },
+    {
+      name: "diamonds",
+      sign: "♦",
+      code: "u+2666 ISOpub",
+      color: "red",
+      image:
+        "https://w7.pngwing.com/pngs/667/791/png-transparent-playing-card-poker-suit-spades-gambling-ace-card-king-leaf-hearts.png"
+    }
+  ];
 
   let numbers = [
     "A",
@@ -45,11 +57,18 @@ window.onload = function() {
   let suit = Math.floor(Math.random() * suits.length);
   let number = Math.floor(Math.random() * numbers.length);
 
+  //creamos una variable newSuit para almacenar el elemento de html que está indicado como id=suit.
+  //mandamos a html el valor de suits[posición suit que es el valor aleatorio de los 4 suits que hemos hecho]
+
   var newSuit = document.getElementById("suit");
-  newSuit.innerHTML = suits[suit];
+  newSuit.innerHTML = suits[suit].sign;
+
+  //AÑADIR COLOR EN CLASS
+  //var newSuit = document.getElementById("suit");
+  //newSuit.innerHTML = suits[suit].color;
 
   var newSuit2 = document.getElementById("suit2");
-  newSuit2.innerHTML = suits[suit];
+  newSuit2.innerHTML = suits[suit].sign;
 
   var newcard = document.getElementById("number");
   newcard.innerHTML = numbers[number];
